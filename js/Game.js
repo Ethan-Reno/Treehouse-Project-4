50bfcae6ac132dc1f3075c0b263388876c2fd5b1
Ethan Reno <epreno@live.ca>
2021-01-20 18:32:08 -0500

    Early changes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,26 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+      this.missed = 0;
+      this.phrases = 
+       ['Absolute power corrupts absolutely',
+        'Verbosity leads to unclear, inarticulate things',
+        'A riddle wrapped up in an enigma',
+        'Let a thousand flowers bloom',
+        'From sea to shining sea'];
+      this.activePhrase = null;
+    }
+};
+
+
+    /**
+     * Selects random phrase from phrases property
+     * @return {Object} Phrase object chosen to be used
+     
+    getRandomPhrase() {
+        return this.phrases[Math.floor(Math.random() * this.phrases.length)];
+     }
+    */
